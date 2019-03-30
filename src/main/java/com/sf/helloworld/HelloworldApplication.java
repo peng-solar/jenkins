a2b3ae8bfc6efa6866e7b81710b218695dcb15d9
peng-solar <peng_solar@163.com>
2019-03-30 20:10:58 +0800

    测试jenkins

---
+++ src/main/java/com/sf/helloworld/HelloworldApplication.java
@@ -0,0 +1,21 @@
+package com.sf.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloworldApplication.class, args);
+    }
+
+    @GetMapping("hello")
+    public String hello(){
+        return "hello world";
+    }
+
+}
